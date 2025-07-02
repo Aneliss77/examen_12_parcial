e79b4967f2e4949777b72628d1d8d8cd8cedb796
Anelisse <anelisseportillo1@gmail.com>
2025-07-02 11:21:42 -0600

    comienzoexamen

---
+++ Libro.java
@@ -0,0 +1,12 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private Double precio;
+
+    public void getPrecio(){
+        
+    }
+    public void getResumen(){
+
+    }
+}
